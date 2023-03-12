9a5497fda437f51dab137e8fc7036e9132cedd8b
Juliana Virgens <julianalv1@hotmail.com>
2023-03-11 21:51:55 -0300

    Create collections.py

---
+++ 01-Introducao_colecao_listas/collections.py
@@ -0,0 +1,25 @@
+idades = [30,39,27,18]
+
+print(idades[1])
+print(idades[2])
+print(idades[3])
+
+idades = [30,39,27,18]
+
+type (idades)
+
+len (idades)
+
+idades
+
+idades.append(15)
+idades
+
+idades [4]
+
+for idade in idades:
+  print(idade)
+
+idades.remove(30)
+
+idades
